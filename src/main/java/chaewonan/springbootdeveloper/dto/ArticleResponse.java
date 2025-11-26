a24ab2a3c7f8814f021b0f40eca25a2bae376e9c
chaeyeon613 <ichaeyeon090@gmail.com>
2025-11-26 16:18:25 +0900

    [refactor] ArticleResonse에likeCount 추가

--- src/main/java/chaewonan/springbootdeveloper/dto/ArticleResponse.java
+++ src/main/java/chaewonan/springbootdeveloper/dto/ArticleResponse.java
@@ -3,14 +3,17 @@ package chaewonan.springbootdeveloper.dto;
 import chaewonan.springbootdeveloper.domain.Article;
 import lombok.Getter;
 
+
 @Getter
 public class ArticleResponse {
 
     private final String title;
     private final String content;
+    private final int likeCount; // 추가!
 
-    public ArticleResponse(Article article) {
+    public ArticleResponse(Article article, int likeCount) {
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.likeCount = likeCount;
     }
 }
