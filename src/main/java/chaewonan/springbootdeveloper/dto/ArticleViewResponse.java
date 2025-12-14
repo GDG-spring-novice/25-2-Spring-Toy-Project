b7fb8def985d36ed44dc29ce1759770d8aa19a46
anchaewon <anchaewon327@naver.com>
2025-12-15 07:12:27 +0900

    commit

--- src/main/java/chaewonan/springbootdeveloper/dto/ArticleViewResponse.java
+++ src/main/java/chaewonan/springbootdeveloper/dto/ArticleViewResponse.java
@@ -1,20 +1,24 @@
 package chaewonan.springbootdeveloper.dto;
 
-
 import chaewonan.springbootdeveloper.domain.Article;
+import chaewonan.springbootdeveloper.domain.ArticleImage;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @Getter
 public class ArticleViewResponse {
+
     private Long id;
     private String title;
     private String content;
     private LocalDateTime createdAt;
     private String author;
+    private List<String> imageUrls;
 
     public ArticleViewResponse(Article article) {
         this.id = article.getId();
@@ -22,5 +26,10 @@ public class ArticleViewResponse {
         this.content = article.getContent();
         this.createdAt = article.getCreatedAt();
         this.author = article.getAuthor();
+        this.imageUrls = article.getImages() == null
+                ? List.of()
+                : article.getImages().stream()
+                .map(ArticleImage::getImageUrl)
+                .collect(Collectors.toList());
     }
 }
